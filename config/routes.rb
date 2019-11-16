a8a3fe6fe68dee6c9a0cf816fd756dfc8d8ce765
Afani Martin <afanimartin@localhost.localdomain>
2019-11-16 13:05:01 +0300

    Test user authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :users
+  # devise_for :users
+  devise_for :users do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
   root "users#index"
 end
